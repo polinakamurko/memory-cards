4189b9fa4185d6442a976f06a04ce53bcb72fc99
Polina Kamurko <polinakamurko@icloud.com>
2019-05-22 14:26:42 +0300

    Initial commit

---
+++ MemoryCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryCards
+//
+//  Created by Полина Камурко on 22/05/2019.
+//  Copyright © 2019 Полина Камурко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
